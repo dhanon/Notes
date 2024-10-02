7103da30bfc63e03aef78200a1b84227fdc464c4
“dhanon” <dhanon68@gmail.com>
2024-10-03 00:22:39 +0600

    Complete Note

--- MyNotes/Views/ListNotesViewController.swift
+++ MyNotes/Views/ListNotesViewController.swift
@@ -172,5 +172,25 @@ extension ListNotesViewController: UISearchControllerDelegate, UISearchBarDelega
 
 //MARK:- NSFetchedResultController Delegate
 extension ListNotesViewController: NSFetchedResultsControllerDelegate {
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
     
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            tableView.insertRows(at: [newIndexPath!], with: .automatic)
+        case .delete:
+            tableView.deleteRows(at: [indexPath!], with: .automatic)
+        case .update:
+            tableView.reloadRows(at: [indexPath!], with: .automatic)
+        case .move:
+            tableView.moveRow(at: indexPath!, to: newIndexPath!)
+        default: tableView.reloadData()
+        }
+        refreshCountLbl()
+    }
 }
